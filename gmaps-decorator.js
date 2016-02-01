4823c92461cbceb1b539358da11f37afc1afa778
Amenadiel <amenadiel@gmail.com>
2016-02-01 12:06:30 -0300

    fixes eslint warnings on gmaps-decorator

--- gmaps-decorator.js
+++ gmaps-decorator.js
@@ -1,8 +1,8 @@
-(function(root, factory) {
+(function (root, factory) {
 
     if (typeof define === "function" && define.amd) {
         // AMD (+ global for extensions)
-        define(function() {
+        define(function () {
             return factory();
         });
     } else if (typeof module !== 'undefined' && typeof exports === "object") {
@@ -12,39 +12,26 @@
         // Browser
         root.gmaps = factory();
     }
-}(this, function() {
+}(this, function () {
 
-    var gmapsDecorator = function(gmaps) {
+    var gmapsDecorator = function (gmaps) {
         if (gmaps.MVCObject) {
 
 
-            gmaps.Rectangle.prototype.toBox = function() {
-                var bounds = this.getBounds();
-                var SW = bounds.getSouthWest();
-                var NE = bounds.getNorthEast();
-                return ['BOX(', SW.lng(), ' ', SW.lat(), ' , ', NE.lng(), ' ', NE.lat(), ')'].join('');
-            };
             /**********************************************
              *
              * MVCObject Prototipes
              *
              *********************************************/
 
-            gmaps.MVCObject.prototype.getPosition = function() {
+            gmaps.MVCObject.prototype.getPosition = function () {
                 return this.get('position');
             };
 
-
-
-
-
-            gmaps.MVCObject.prototype.getGeometry = function() {
-                var self = this;
-                return new gmaps.Data.Point(self.get('position'));
+            gmaps.MVCObject.prototype.getGeometry = function () {
+                var _this = this;
+                return new gmaps.Data.Point(_this.get('position'));
             };
-
-
-
 
 
             /*********************************
@@ -52,82 +39,77 @@
              * Rectangle Prototipes
              *
              *********************************/
-            gmaps.Rectangle.prototype.getCenter = function() {
+            gmaps.Rectangle.prototype.toBox = function () {
+                var bounds = this.getBounds();
+                var SW = bounds.getSouthWest();
+                var NE = bounds.getNorthEast();
+                return ['BOX(', SW.lng(), ' ', SW.lat(), ' , ', NE.lng(), ' ', NE.lat(), ')'].join('');
+            };
+
+            gmaps.Rectangle.prototype.getCenter = function () {
                 return this.getBounds().getCenter();
             };
 
-            gmaps.Rectangle.prototype.getPath = function() {
-                return [this.getBounds().getCenter()];
-            };
 
             /*********************************
              *
              * LatLng Prototipes
              *
              *********************************/
-            gmaps.LatLng.prototype.getCenter = function() {
+            gmaps.LatLng.prototype.getCenter = function () {
                 return this;
             };
-            gmaps.LatLng.prototype.getPosition = function() {
+            gmaps.LatLng.prototype.getPosition = function () {
                 return this;
             };
 
-            gmaps.LatLng.prototype.toWkt = function() {
+            gmaps.LatLng.prototype.toWkt = function () {
                 return "POINT(" + Number(this.lng()).toFixed(6) + " " + Number(this.lat()).toFixed(6) + ")";
             };
 
-            gmaps.LatLng.prototype.toJson = function() {
+            gmaps.LatLng.prototype.toJson = function () {
                 return {
                     type: 'Point',
                     coordinates: [1 * Number(this.lng()).toFixed(6), 1 * Number(this.lat()).toFixed(6)]
                 };
             };
 
-
-
-
-
-
             /*********************************
              *
              * Marker Prototipes
              *
              *********************************/
-            gmaps.Marker.prototype.getCenter = function() {
+            gmaps.Marker.prototype.getCenter = function () {
                 return this.getPosition();
             };
 
-            gmaps.Marker.prototype.getPath = function() {
+            gmaps.Marker.prototype.getPath = function () {
                 return [this.getPosition()];
             };
 
-            gmaps.Marker.prototype.setProperty = function(key, value) {
+            gmaps.Marker.prototype.setProperty = function (key, value) {
                 return this.set(key, value);
             };
 
-            gmaps.Marker.prototype.getProperty = function(key) {
+            gmaps.Marker.prototype.getProperty = function (key) {
                 return this.element.get(key);
             };
-
-
 
             /*********************************
              *
              * Polyline Prototipes
              *
              *********************************/
-            gmaps.Polyline.prototype.getBounds = function() {
+            gmaps.Polyline.prototype.getBounds = function () {
                 var bounds = new gmaps.LatLngBounds();
-                this.getPath().forEach(function(element, index) {
+                this.getPath().forEach(function (element, index) {
                     bounds.extend(element);
                 });
                 return bounds;
             };
-            gmaps.Polyline.prototype.getCenter = function() {
+            gmaps.Polyline.prototype.getCenter = function () {
                 return this.getBounds().getCenter();
             };
-
-
 
 
             /*********************************
@@ -136,32 +118,28 @@
              *
              *********************************/
 
-            gmaps.Polygon.prototype.getBounds = function() {
+            gmaps.Polygon.prototype.getBounds = function () {
                 var bounds = new gmaps.LatLngBounds();
-                this.getPath().forEach(function(element, index) {
+                this.getPath().forEach(function (element, index) {
                     bounds.extend(element);
                 });
                 return bounds;
             };
 
-            gmaps.Polygon.prototype.getCenter = function() {
+            gmaps.Polygon.prototype.getCenter = function () {
                 return this.getBounds().getCenter();
             };
-            gmaps.Polygon.prototype.getPosition = function() {
+            gmaps.Polygon.prototype.getPosition = function () {
                 return this.getBounds().getCenter();
             };
-            gmaps.Polygon.prototype.setProperty = function(key, value) {
+            gmaps.Polygon.prototype.setProperty = function (key, value) {
                 return this.set(key, value);
             };
-            gmaps.Polygon.prototype.setProperties = function(properties) {
+            gmaps.Polygon.prototype.setProperties = function (properties) {
                 return this.setOptions(properties);
             };
 
-            gmaps.Polygon.prototype.name_changed = function() {};
-            gmaps.Polygon.prototype.center_changed = function() {};
-
-
-            gmaps.Polygon.prototype.getArea = function() {
+            gmaps.Polygon.prototype.getArea = function () {
 
                 return gmaps.geometry.spherical.computeArea(this.getPath());
             };
@@ -171,25 +149,16 @@
              * Circle Prototipes
              *
              *********************************/
-            gmaps.Circle.prototype.setProperty = function(key, value) {
+            gmaps.Circle.prototype.setProperty = function (key, value) {
                 return this.set(key, value);
             };
 
-
-
-            gmaps.Circle.prototype.getArea = function() {
-                var self = this,
-                    radius = self.getRadius();
-                return Math.PI * radius ^ 2;
+            gmaps.Circle.prototype.getArea = function () {
+                var _this = this,
+                    radius = _this.getRadius();
+                return Math.PI * Math.pow(radius, 2);
 
             };
-
-
-
-
-
-
-
 
         }
 
@@ -197,6 +166,6 @@
     };
 
 
-    return gmapsDecorator
+    return gmapsDecorator;
 
 }));
